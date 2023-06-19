f7537a50616f0c8abb855c6e984c47480786b9a2
TrungKien Nguyen <trungkien.nguyen@tui.co.uk>
2023-06-19 15:23:40 +0700

    fix cors socket.io-client

--- client/src/context/socket.js
+++ client/src/context/socket.js
@@ -1,5 +1,14 @@
 import React from "react";
 import socketIo from "socket.io-client";
 
-export const socketServer = socketIo.connect(process.env.REACT_APP_SOCKET_ENDPOINT);
+export const socketServer = socketIo.connect(process.env.REACT_APP_SOCKET_ENDPOINT,
+    {
+        transportOptions: {
+          polling: {
+            extraHeaders: {
+                'ngrok-skip-browser-warning': 69420,
+            }
+          }
+        }
+      });
 export const SocketContext = React.createContext();

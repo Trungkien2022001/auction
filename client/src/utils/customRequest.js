4d5a50e66a4cf534069ba9aff888c26098f51862
TrungKien Nguyen <trungkien.nguyen@tui.co.uk>
2023-06-19 15:13:55 +0700

    test vercel

--- client/src/utils/customRequest.js
+++ client/src/utils/customRequest.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 export async function get(url, currentUser){
     const result = await axios.get(url, {
         headers: {
+            'ngrok-skip-browser-warning': 69420,
             'x-key': currentUser.email,
             'x-access-token': currentUser.token,
         }
@@ -13,6 +14,7 @@ export async function get(url, currentUser){
 export async function post(url, data, currentUser){
     const result = await axios.post(url, data, {
         headers: {
+            'ngrok-skip-browser-warning': 69420,
             'x-key': currentUser.email,
             'x-access-token': currentUser.token,
         }

a22f77244208d1b1b75b4cc19cb9d9d3c12a8c68
nguyenkien2022001 <nguyenkien2022001@gmail.com>
2024-09-14 00:47:15 +0700

    Test: Monorepo test

--- packages/core/src/main.ts
+++ packages/core/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  awesomeFn()
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
